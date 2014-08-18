3c48cb51f7d65c55e435686994243550069a38e5
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-08-18 16:11:37 +0300

    Update setup files

--- bash.sh
+++ bash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -t -i -v $(pwd)/rails_app:/rails_app --name rails yoihito/rails /bin/bash
+docker run -t -i -v $(pwd)/rails_app:/rails_app --name rails --link postgres:postgres yoihito/rails /bin/bash
 docker rm rails

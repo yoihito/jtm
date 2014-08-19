88aab0683f07589326d42691eff47d0062347604
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-08-19 13:37:04 +0300

    Fix bash

--- bash.sh
+++ bash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -t -i -v $(pwd)/rails_app:/rails_app --name rails --link postgres:postgres yoihito/rails /bin/bash
+docker run -t -i -p 5000:3000 -v $(pwd)/rails_app:/rails_app --name rails --link postgres:postgres yoihito/rails /bin/bash
 docker rm rails

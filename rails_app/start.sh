54c0939c3f96bceb3e528eca5c4f0cc02cefe425
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-08-20 15:29:23 +0300

    Close auth integration

--- rails_app/start.sh
+++ rails_app/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd app
 rm tmp/pids/server.pid
-export DEV_POSTGRES_IP=172.17.0.11
+export DEV_POSTGRES_IP=postgres
 export DEV_POSTGRES_USER=postgres
 bundle install
 bin/rake db:setup

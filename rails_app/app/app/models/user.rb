23e48fbdc4c60434cfeb3d334b85a4c616bcab1f
Vadim Gribanov <v1m@tut.by>
2014-08-27 17:32:04 +0300

    Add roles to user

--- rails_app/app/app/models/user.rb
+++ rails_app/app/app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  ROLES = %w[user admin root]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tests, as: :author
   has_many :user_answers
   has_many :passed_tests, through: :user_answers, class_name: "Test"
+
+  def roles=(roles)
+  	self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+  end
+
+  def roles
+  	ROLES.reject do |r|
+  		((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
+  	end
+  end
+
+  def is?(role)
+  	roles.include?(role.to_s)
+  end
+
+
+
 end

a1c44d3ac209f764edcbf7deda94f96b51cc2438
Vadim Gribanov <v1m@tut.by>
2014-08-27 12:31:26 +0300

    Fix syntax

--- rails_app/app/db/seeds.rb
+++ rails_app/app/db/seeds.rb
@@ -1,10 +1,10 @@
-unless Slide.any? do 
+unless Slide.any?
 	Slide.create(question:'How are you dick?', description:'Dick - penis');
 	Slide.create(question:'How are you Nigga?', description:'Nigga - nigger');
 	Slide.create(question:'How are you alchoholic?', description:'Fuck you!');
 end
 
-unless Test.any? do
+unless Test.any?
 	test = Test.create(title:'Awesome test')
 	test.slides << Slide.all
 end

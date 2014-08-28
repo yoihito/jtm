eec408771b78cd976a0c2f1d6f48af0a11544895
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-08-28 17:10:30 +0300

    Add csrf token for each ajax request

--- rails_app/app/app/frontend/global/javascripts/utils/ajax.js
+++ rails_app/app/app/frontend/global/javascripts/utils/ajax.js
@@ -3,11 +3,17 @@
     var app = window.app;
 
     app.utils.ajax = function( type, url, data ) {
+        var csrf_param, csrf_token;
+
         if ( !data ) {
             data = url;
             url = type;
             type = 'post';
         }
+
+        csrf_param = $( 'meta[name=csrf-param]' ).attr( 'content' );
+        csrf_token = $( 'meta[name=csrf-token]' ).attr( 'content' );
+        data[ 'data' ][ csrf_param ] = csrf_token;
 
         data[ 'url' ] = url;
         data[ 'type' ] = type;

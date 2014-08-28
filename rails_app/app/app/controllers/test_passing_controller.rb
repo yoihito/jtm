d8b7758d07fed9fa4edd32dc0e1e0ec67832f9c7
Vadim Gribanov <v1m@tut.by>
2014-08-28 17:48:34 +0300

    Add answers saving

--- rails_app/app/app/controllers/test_passing_controller.rb
+++ rails_app/app/app/controllers/test_passing_controller.rb
@@ -4,9 +4,23 @@ class TestPassingController < ApplicationController
 	end
 
 	def save
-
+		#binding.pry
+		@answers = UserAnswers.find_or_initialize_by(user_id: current_user.id, test_id: params[:id])
+		@answers.answers = answers_params
+		#@answers.user_id = current_user.id
+		#@answers.test_id = params[:id]
+		logger.debug "#{@answers.inspect}"
+		@answers.save
 	end
 
 	def result
 	end
+
+private
+
+	def answers_params
+		params.require(:slides)
+	end
+
+
 end
--- rails_app/app/config/routes.rb
+++ rails_app/app/config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   get 'profile/index'
-  get 'tests/pass/:id', to: 'test_passing#get'
 
-  devise_for :users
-  resources :questions
+  scope '/tests' do 
+  	get 'pass/:id', to: 'test_passing#get'
+  	post 'save/:id', to: 'test_passing#save'
+  	get 'result/:id', to: 'test_passing#result'
+  end
 
-  resources :tests
+  scope '/page' do 
+  	get 'index', to: 'page#index'
+  end
 
   get 'market/show'
 
-  get 'page/index'
 
+  devise_for :users
+  resources :questions
+  resources :tests
   resources :users
   root 'page#index'
 end

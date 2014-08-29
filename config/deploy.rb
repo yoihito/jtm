a6d321bdb501d95e8cca7a3fd944c783fbdd9a69
Vadim Gribanov <v1m@tut.by>
2014-08-29 14:17:22 +0300

    Change deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,9 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
 
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{public/uploads}
+
 after "deploy", "deploy:cleanup"
 
 set :keep_releases, 5
@@ -33,15 +36,15 @@ namespace :deploy do
 
   task :setup_config do
     on roles(:app), in: :sequence, wait: 5 do
-      # sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
-      # sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{fetch(:application)}"
-  #    run "mkdir -p #{shared_path}/config"
-      put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+      sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
+      sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{fetch(:application)}"
+     # run "mkdir -p #{shared_path}/config"
+      # put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
       puts "Now edit the config files in #{shared_path}."
     end
   end
 
-  after "deploy:starting", "deploy:setup_config"
+  # after "deploy:starting", "deploy:setup_config"
 
   # task :symlink_config do
   #   on roles(:app), in: :sequence, wait: 5 do

fd5a51036cf4170c5f3321a37817f35956ea1bcc
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-08-29 12:48:01 +0300

    Fix spelling

--- rails_app/app/app/frontend/global/javascripts/utils/routes.js
+++ rails_app/app/app/frontend/global/javascripts/utils/routes.js
@@ -4,8 +4,8 @@
 
     app.utils.route_parse = function( str, obj ) {
         var
-            isCustomType = _( str ).isString(),
-            out = ( isCustomType ) ? str : str[ 0 ];
+            isDefaultType = _( str ).isString(),
+            out = ( isDefaultType ) ? str : str[ 0 ];
 
         _( obj ).forEach(function( value, key ) {
             var regexp = new RegExp( ':' + key + '(\\/?)', 'g' );
@@ -13,7 +13,7 @@
             out = out.replace( regexp, value + '$1' );
         });
 
-        return ( isCustomType ) ? out : [ out, str[ 1 ] ];
+        return ( isDefaultType ) ? out : [ out, str[ 1 ] ];
     };
 
 })( window, jQuery );

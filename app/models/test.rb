56d8030d4f83efb50bf95e7703ac2ffb4421cc27
Vadim Gribanov <v1m@tut.by>
2014-09-03 13:53:20 +0300

    Revert "Rename table user_answers to users_tests"
    This reverts commit 6364dc5fc0a1f6cac3808ff20987518ac2d64b9b.

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,6 @@ class Test < ActiveRecord::Base
 	has_and_belongs_to_many :slides
 	accepts_nested_attributes_for :slides
 	belongs_to :author, polymorphic: true
-	has_many :users_tests
+	has_many :user_answers
 	has_many :users, through: :user_answers
 end
--- app/models/users_tests.rb
+++
@@ -1,4 +0,0 @@
-class UsersTests < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :test
-end

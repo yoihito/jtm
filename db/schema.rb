70ae1862d94224a273d8ccdfd0693a746bf572d1
Vadim Gribanov <v1m@tut.by>
2014-09-03 18:47:27 +0300

    Add avatar to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903151356) do
+ActiveRecord::Schema.define(version: 20140903154551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,10 @@ ActiveRecord::Schema.define(version: 20140903151356) do
     t.string   "name"
     t.json     "profile",                default: {}
     t.string   "language",               default: "en"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

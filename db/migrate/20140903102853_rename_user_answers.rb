1667b4146a2bc53bcb8d486340f8561bf14cfb0e
Vadim Gribanov <v1m@tut.by>
2014-09-03 13:53:20 +0300

    Revert "Rename table user_answers to users_tests"
    This reverts commit 6364dc5fc0a1f6cac3808ff20987518ac2d64b9b.

--- db/migrate/20140903102853_rename_user_answers.rb
+++
@@ -1,9 +0,0 @@
-class RenameUserAnswers < ActiveRecord::Migration
-  def up
-  	rename_table :user_answers, :users_tests
-  end
-
-  def down
-  	rename_table :users_tests, :user_answers
-  end
-end

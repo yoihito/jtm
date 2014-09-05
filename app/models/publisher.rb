d89bf7580d64b44f8bcd709a4f4d61b83640df8c
Vadim Gribanov <v1m@tut.by>
2014-09-05 13:52:37 +0300

    Add avatar to public

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,4 +1,6 @@
 class Publisher < ActiveRecord::Base
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: ':style/missing.png'
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 	has_many :tests, as: :author
 	has_many :likes, as: :entity
 	has_many :fans, through: :likes, source: :author, source_type: 'User'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905072749) do
+ActiveRecord::Schema.define(version: 20140905104920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,8 +46,12 @@ ActiveRecord::Schema.define(version: 20140905072749) do
     t.boolean  "has_avatar"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "description", default: ""
-    t.json     "profile",     default: {}
+    t.string   "description",         default: ""
+    t.json     "profile",             default: {}
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "slide_translations", force: true do |t|

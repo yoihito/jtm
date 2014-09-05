531d3728df86c5b449030dc4e7f7c1aa4edd84b2
Vadim Gribanov <v1m@tut.by>
2014-09-05 16:21:47 +0300

    Add user profile editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
+
   end
+
+  def edit
+  	
+  end
+
+  def update
+  	@user.update(user_params)
+  end
+
+private
+
+  def set_user
+  	@user = User.find(params[:id])
+  end
+
+  def user_params
+  	params.require(:user).permit(:name, :gender, :birthday, :password)
+  end
+
 end

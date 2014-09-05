a4d7e281ff259d28c4acb21b00c83ef24be55654
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-09-05 14:08:32 +0300

    Show uploaded images for test creating

--- app/frontend/blocks/test/index.js
+++ app/frontend/blocks/test/index.js
@@ -11,18 +11,29 @@
                 $( '.js-newtest-cover' ).trigger( 'click' );
             })
             .on( 'change', '.js-newtest-cover', function( e ) {
-                var file = this.files[ 0 ];
+                var
+                    $form = $( this ).closest( 'form' ),
+                    file = this.files[ 0 ];
 
                 // @TODO: Clearing input file
                 // this.replaceWith( this.val( '' ).clone( true ) );
 
-                $( '.js-newtest-nameCover' ).text( file.name );
+                app.utils.file.getBase64( FileReader, file, function( data ) {
+                    $form.find( '.js-newtest-srcCover' ).attr( 'src', data );
+                });
             })
             .on( 'tap', '.js-newtest-addPic', function( e ) {
                 e.preventDefault();
 
                 $( '.js-newtest-slidePic_' + this.getAttribute( 'rel' ) ).trigger( 'click' );
             })
+            .on( 'change', '.js-newtest-slidePic', function( e ) {
+                var index = this.getAttribute( 'rel' );
+
+                app.utils.file.getBase64( FileReader, this.files[ 0 ], function( data ) {
+                    $( '.js-newtest-slideImg_' + index ).attr( 'src', data );
+                });
+            });
 
     });
 

b723134d7fef7e65be0a5659c38191488049ba13
Vadim Gribanov <v1m@tut.by>
2014-09-07 03:22:21 +0300

    Add updating and removing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	before_action :set_comment, only: [:update, :destroy]
 	
 	def create
 		@comment = Comment.new(comment_params)
@@ -7,15 +8,26 @@ class CommentsController < ApplicationController
 	end
 
 	def update
+		@comment.update(update_comment_params)
 	end
 
 	def destroy
+		@comment.destroy
 	end
 
 private
 	
-	def comment_params
+	def set_comment
+		@comment = Comment.find(params[:id])
+	end
+
+
+	def create_comment_params
 		params.require(:comment).permit(:content,:entity_id, :entity_type)
+	end
+
+	def update_comment_params
+		params.require(:comment).permit(:content)
 	end
 
 

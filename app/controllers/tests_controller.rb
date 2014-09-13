16878e8401a10d2f30597dc6048e19758e8ceb1e
Vadim Gribanov <v1m@tut.by>
2014-09-13 16:14:39 +0300

    Add spaces in tests

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -11,7 +11,10 @@ class TestsController < ApplicationController
 		end
 		@q = Test.search(params[:q])
 		if !current_user.nil? && current_user.passed_tests.count>0
-			@tests = @q.result.includes([:user_answers, :translations, :author, comments: [:author]]).where('id not in (?)',current_user.passed_tests.map{|x| x.id}.join(',')).order(@order)
+			@tests = @q.result
+					.includes([:user_answers, :translations, :author, comments: [:author]])
+					.where('id not in (?)', current_user.passed_tests.map{|x| x.id}.join(','))
+					.order(@order)
 		else
 			@tests = @q.result.includes([:user_answers, :translations, :author, comments: [:author]]).order(@order)
 		end

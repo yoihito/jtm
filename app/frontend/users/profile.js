cbd7fada9068476ef136dd556905e9204cc75e00
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-09-14 17:45:23 +0300

    Fix issues in user page
    Fix #36

--- app/frontend/users/profile.js
+++ app/frontend/users/profile.js
@@ -14,11 +14,33 @@
                 $pattern = $items.eq( 0 ),
 
                 item_width = $pattern.outerWidth() + parseInt( $pattern.css( 'marginRight' ) ) + parseInt( $pattern.css( 'marginLeft' ) ),
-                list_width = Math.floor( $parent.width() / item_width ) * item_width,
+                count_visible = Math.floor( $parent.width() / item_width ),
+                list_width = count_visible * item_width,
 
-                maxWidth = Math.min( list_width, item_width * $items.length ) - 50;
+                delta_width = -50;
+                delta_height = 60;
+                maxWidth = Math.min( list_width, item_width * $items.length ) + delta_width,
+                maxHeight = delta_height;
 
-            $list.css( { maxWidth: maxWidth } )
+            var
+                height = 0,
+                $elem = $pattern,
+                n = 0;
+
+            while ( $elem.length && n < count_visible ) {
+                var elem_height = $elem.outerHeight();
+
+                maxHeight += elem_height;
+
+                height = elem_height;
+
+                $elem = $elem.next();
+                n++;
+            }
+
+            height += delta_height;
+
+            $list.css( { maxWidth: maxWidth, maxHeight: maxHeight, height: height } )
         };
 
     $(function() {
@@ -26,7 +48,18 @@
         $parent = $( '.user' );
         $lists = $( '.list-cont-user' );
 
-        reuse();
+        var
+            $imgs = $( '.bg-test > img, .img-item-publishers' ),
+            imgs_count = $imgs.length,
+            counter = 0;
+
+        $imgs.on( 'load', function() {
+            counter++;
+
+            if ( counter == imgs_count ) {
+                reuse();
+            }
+        });
 
         var timer_resize;
 

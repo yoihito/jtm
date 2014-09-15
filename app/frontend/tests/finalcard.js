7d56a63d54065b1bf37df6c962b0b77abc9cb8ea
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-09-15 04:07:56 +0300

    Small syntax refactoring

--- app/frontend/tests/finalcard.js
+++ app/frontend/tests/finalcard.js
@@ -1,27 +1,29 @@
 (function( window, $ ) {
+
     var app = window.app;
 
     $(function(){
         var
-            submitComment = function($comment_list,testid, comment) {
-                app.utils.ajax('submit_comment', {id: testid}, {
+            submitComment = function( $comment_list, testid, comment ) {
+                app.utils.ajax( 'submit_comment', { id: testid }, {
                     data: { comment : comment },
-                    success: function(data) {
-                        // console.log( $comment_list )
-                        $comment_list.html(data)
+                    success: function( data ) {
+                        $comment_list.html( data )
                     }
                 });
-            }
+            };
+
         $( document )
-        .on( 'tap', '.js-finalcard-submit-comment', function( e ) {
-            var $this = $(this)
-            e.preventDefault();
-            var comment = { 'content' : $this.parent().find('.js-finalcard-content').val() }
-            submitComment($this.parent().parent().find('.comments-list-finalcard'),$this.attr('data-testid'), comment);
+            .on( 'tap', '.js-finalcard-submit-comment', function( e ) {
+                e.preventDefault();
 
+                var
+                    $this = $(this),
+                    $parent = $this.parent(),
+                    comment = { 'content' : $parent.find( '.js-finalcard-content' ).val() };
 
-
-        })
+                submitComment( $parent.parent().find( '.comments-list-finalcard' ), $this.attr( 'data-testid' ), comment );
+            });
     });
 
 })( window, jQuery );

724866a887b84f03125560d9b5e910923947fb47
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-09-15 16:39:34 +0300

    Progress bar for avatar uploading

--- app/frontend/users/avatar.js
+++ app/frontend/users/avatar.js
@@ -6,15 +6,54 @@
 
         $( document )
             .on( 'change', '.js-user-avatar', function() {
-                var that = this;
+                var
+                    that = this,
+                    $loader = $( '.loader-avatar' ),
+                    $loader_hand = $loader.find( 'b' );
 
                 app.utils.file.getBase64( FileReader, that.files[ 0 ], function( result ) {
                     app.utils.ajax( 'user_avatar', { id: parseInt( that.getAttribute( 'data-uid' ) ) }, {
                         data: {
                             user: { avatar: result }
                         },
+                        xhr: function() {
+                            var xhr = new window.XMLHttpRequest();
+
+                            //Upload progress
+                            xhr.upload.addEventListener( "progress", function(evt){
+                                if ( evt.lengthComputable ) {
+                                    var
+                                        done = evt.position || evt.loaded,
+                                        total = evt.totalSize || evt.total,
+                                        percent = Math.floor( done / total * 1000 ) / 10;
+
+                                    $loader_hand.css({ width: percent + "%" });
+                                } else {
+                                    console.warn();
+                                }
+                            }, false);
+
+                            //Download progress
+                            xhr.addEventListener( "progress", function(evt){
+                                if ( evt.lengthComputable ) {
+                                    var percentComplete = evt.loaded / evt.total;
+                                    //Do something with download progress
+
+                                    // console.log( 'download' );
+                                } else {
+                                    console.warn();
+                                }
+                            }, false);
+
+                            return xhr;
+                        },
+
+                        beforeSend: function() {
+                            $loader.css({ opacity: 1 });
+                        },
                         success: function() {
                             $( '.img-info-user' ).attr( 'src', result );
+                            $loader.css({ opacity: 0 });
                         }
                     } );
                 });

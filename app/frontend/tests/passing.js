9601be44c6f023422f189dc9bcc560b12af66105
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2014-09-17 21:50:55 +0300

    Fix tests passing

--- app/frontend/tests/passing.js
+++ app/frontend/tests/passing.js
@@ -18,10 +18,18 @@
             loadFinalCard = function( $list ) {
                 var $content = $list.parent();
 
-                app.utils.ajax( 'test_result', { id: $list.attr( 'data-testid' ) }, {
+                var tid = parseInt( $list.attr( 'data-testid' ) );
+
+                app.utils.ajax( 'test_result', { id: tid }, {
                     success: function( data ) {
                         $content.html( data );
                         app.fns.reinit();
+
+                        if ( app.blocks && app.blocks.users && app.blocks.users.reuse ) {
+                            app.blocks.users.reuse( $( '.user' ), $( '.list-cont-user' ) );
+                        }
+
+                        History.pushState({type: 'results', test: {id: tid}}, 'Result', app.utils.route_url( 'test_result', { id: tid } ) );
                     }
                 });
             };
